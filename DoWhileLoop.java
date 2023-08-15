d0929a67b4f76bbfc81aa450236bb77688a19a80
mauryanitish <m.nitishkumar786@gmail.com>
2023-08-15 16:23:37 +0530

    15-aug-23

---
+++ DoWhileLoop.java
@@ -0,0 +1,11 @@
+public class DoWhileLoop {
+    public static void main(String[] args)
+    {
+        int i = 1;
+        do{
+            System.out.print("hii "+i);
+            i++;
+        }
+        while(i<2);
+    }
+}
---
+++ ForLoop.java
@@ -0,0 +1,8 @@
+public class ForLoop {
+    public static void main(String[] args) {
+        for(int i = 0; i < 4;  ){
+            i++;
+            System.out.println("HEllo");
+        }
+    }
+}
